969089cbeb6df86de34d02fa65fedde937262f09
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-06-05 22:20:20 -0400

    add better hover

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,17 +8,17 @@ class Footer extends Component {
     render() {
         return (
             <div class="row justify-content-md-left foot-element p-5">
-                <div class="col-sm-6">
-                    <p>
-                        <h3 class="foot-section">Attribution</h3>
-                        <p>Free SVG Backgrounds and Patterns by <FontAwesomeIcon icon={faArrowRight} /> <a href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">SVGBackgrounds.com</a></p>    
-                    </p>
+                <div class="col-sm-8">
                     <p>
                         <h3 class="foot-section">Contact</h3>
-                        <p>if you're actually reading this way down here, lets talk websites! <FontAwesomeIcon icon={faArrowRight} /> <a href="mailto:dev@teresemihalcin.com">dev@teresemihalcin.com</a></p>
+                        <p>If you're actually reading this way down here, you can reach out to me about either band things ot website things <FontAwesomeIcon icon={faArrowRight} /> <a href="mailto:dev@teresemihalcin.com">dev@teresemihalcin.com</a></p>
+                    </p>
+                    <p>
+                        <h4 class="foot-section">Attribution</h4>
+                        <p>Free SVG Backgrounds and Patterns by <FontAwesomeIcon icon={faArrowRight} /> <a href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">SVGBackgrounds.com</a></p>    
                     </p>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-4">
                     <img src="/images/lunker.png" alt="lunker logo" class="foot-logo float-end"></img>
                 </div>  
             </div>
